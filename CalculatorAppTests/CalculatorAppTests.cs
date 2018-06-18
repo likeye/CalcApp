9d9b1b605a38fd408d7fd0907bc1f436716c93df
Likeye <wozniakdawid.y@gmail.com>
2018-06-18 23:52:29 +0200

    Add project files.

---
+++ CalculatorAppTests/CalculatorAppTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorApp;
+using NUnit.Framework;
+
+namespace CalculatorAppTests
+{
+    public class CalculatorAppTests
+    {
+        readonly Calculator _calc = new Calculator();
+        [Test]
+        public void Add_AddsTwoPositiveNumbers_Calculated()
+        {
+            var sum = _calc.Add(2, 2);
+            Assert.AreEqual(4, sum);
+        }
+
+        [Test]
+        public void Add_AddsTwoDifferentNumbers_Calculated()
+        {
+            var sum = _calc.Add(-4, 8);
+            Assert.AreEqual(sum, 4);
+        }
+
+        [Test]
+        public void Add_AddsTwoNegativeNumbers_Calculated()
+        {
+            var sum = _calc.Add(-4, -2);
+            Assert.AreEqual(sum, -6);
+        }
+    }
+}
